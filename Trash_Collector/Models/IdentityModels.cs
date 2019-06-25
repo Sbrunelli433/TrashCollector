b02889f72853326515c3ade10fb295f618f36fb2
Sbrunelli433 <brunes46@gmail.com>
2019-06-25 14:19:18 -0500

    created my customers model & controller along with my employees model and controller. Both models have just Primary Key Id set up.

--- Trash_Collector/Models/IdentityModels.cs
+++ Trash_Collector/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Trash_Collector.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
