ac16df99216ab79d5123b87daa6a21f081b78786
Sbrunelli433 <brunes46@gmail.com>
2019-08-31 11:30:36 -0500

    cleaned up Employee model.

--- Trash_Collector/Models/Employee.cs
+++ Trash_Collector/Models/Employee.cs
@@ -26,29 +26,10 @@ namespace Trash_Collector.Models
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Display(Name = "Email Address")]
-        public string EmailAddress { get; set; }
-
         [Display(Name = "Username")]
         public string Username { get; set; }
 
         [Display(Name = "Zipcode")]
         public int Zipcode { get; set; }
-
-        [Display(Name = "Current Date")]
-        public DateTime? CurrentDate { get; }
-
-        [Display(Name = "Confirm Pickup")]
-        public bool ConfirmPickup { get; set; }
-        [Display(Name = "Confirm Extra Pickup")]
-        public bool ConfirmExtraPickup { get; set; }
-
-        [Display(Name = "Charge to Bill")]
-        public double ChargeToBill { get; set; }
-        public IEnumerable<Employee> Employees { get; set; }
-
-        [Display(Name ="Today's Pickups")]
-        public List<object> PickUpThisDay { get; set; }
-
     }
 }
